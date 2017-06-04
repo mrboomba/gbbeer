89207f791d1384c28cd453d2e8dae1f35342e38d
mrboomba <mrboomba161@gmail.com>
2017-06-05 00:13:48 +0700

    clean

--- app/router/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = (() => {
-  const express = require('express');
-  const router = express.Router();
-  const debug = require('debug')('router setting');
-  debug('setting up router');
-  router.use('/api', require('./api'));
-  router.use('/', require('./views'));
-  return router;
-})();
