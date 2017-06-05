0f9e8273a4a72f6938d4cb1fc341c3ab3f0fee6c
gminemini <mine_wongteeravit@hotmail.com>
2017-06-06 03:22:57 +0700

    set root and html index.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { provideRoutes} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { BeerComponent } from './beer/beer.component';
+import { CartComponent } from './cart/cart.component';
+
+const appRoutes: Routes =  [
+  {path:'home', component: HomeComponent},
+  {path:'beer', component: BeerComponent},
+  {path:'cart', component: CartComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    BeerComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
