03425b5c886a30903e37761537fc2aa20e437c16
gminemini <mine_wongteeravit@hotmail.com>
2017-06-17 00:33:53 +0700

    filter

--- src/app/beer/beer.component.ts
+++ src/app/beer/beer.component.ts
@@ -13,13 +13,22 @@ import {RegisterService} from '../services/register.service';
 export class BeerComponent implements OnInit {
 
  private allProduct ;
+ private colorProduct;
+ private countryProduct;
+ private allProductTemp;
+ private price
 
   constructor(private productService:ProductService,private reviewService:ReviewService,private router: Router) {
-  this.productService.getAllproduct().subscribe(data => this.getAllproduct(data)) }
+    this.colorProduct = 'All'
+    this.countryProduct = 'All'
+  
+   this.productService.getAllproduct().subscribe(data => this.getAllproduct(data)) 
+  }
 
   getAllproduct(data) {
   console.log(JSON.parse(data._body))
   this.allProduct  = JSON.parse(data._body)
+  this.allProductTemp = JSON.parse(data._body)
   }
 
   ngOnInit() {
@@ -42,5 +51,88 @@ public sentIDToCart(id :String) {
   console.log(id)
   console.log("id")
   }
+
+    compare(a,b) {
+
+  if (a.name < b.name)
+    return -1;
+  if (a.name > b.name)
+    return 1;
+  return 0;
+}
+compare2(a,b) {
+
+  if (a.price < b.price)
+    return -1;
+  if (a.price > b.price)
+    return 1;
+  return 0;
+}
+
+   getFilter(){
+    let temp = this.allProductTemp
+    this.allProduct = new Array()
+    if(this.colorProduct==='All'&&this.countryProduct==='All'){
+    for(let beer of temp){
+    if(beer.price <=this.price){
+              this.allProduct.push(beer)}
+        
+      }
+    }
+    else if(this.colorProduct!='All'&&this.countryProduct!='All'){
+    for(let beer of temp){
+       if(beer.color === this.colorProduct && beer.country === this.countryProduct &&beer.price <=this.price) {
+          this.allProduct.push(beer)
+        }
+      }
+    }
+    else if(this.colorProduct==='All'){
+    for(let beer of temp){
+        if(beer.country === this.countryProduct &&beer.price <=this.price) {
+          this.allProduct.push(beer)
+        }
+      }
+    }
+    else if( this.countryProduct ==='All'){
+    for(let beer of temp){
+        if(beer.color === this.colorProduct &&beer.price <=this.price) {
+          this.allProduct.push(beer)
+        }
+      }
+    }
+    
+   }
+
+   getCountry(country:String) {
+
+   this.countryProduct = country
+   this.getFilter()
+
+   }
+
+   getColor(color:String) {
+   this.colorProduct = color;
+   this.getFilter();
+   }
+  
+   getSortBy(type:String) {
+  
+    if(type==='Name')
+   this.allProduct.sort(this.compare)
+   else if(type==='Price')
+   this.allProduct.sort(this.compare2)
+   }
+
+   rangeValueChanged(value :any) {
+     
+    this.price = value
+    console.log(value)
+    this.getFilter()
+     
+}
+
+
+
+ 
 
 }
